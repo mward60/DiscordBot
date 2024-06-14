28ce4f7de9548cbab6cbae17b417b639000b4821
cbwolfe94 <cbwolfe94@hotmail.com>
2024-06-14 11:42:50 -0500

    Move some code to guild.py to unclutter main file

--- src/guild/guild.py
+++ src/guild/guild.py
@@ -1,1 +1,14 @@
-# from discord import 
+from discord import app_commands, Interaction, Intents, Client 
+
+def log_bot_guild_presence(discord_client: Client) -> None:
+    """ Logs the bot's guild presence
+
+    Args:
+        discord_client (Client): Discord client
+    """
+    for guild in discord_client.guilds:
+        print(f'{discord_client.user.name} is connected to {guild.name}.')
+        print(f'Server Members ({len(guild.members)}):')
+        
+        for member in guild.members:
+            print(f'- {member.name}')
