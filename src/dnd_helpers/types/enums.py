3e64ec8c1e2c5751ace831f1de4bb46840d21321
cbwolfe94 <cbwolfe94@hotmail.com>
2024-06-15 21:28:36 -0500

    Add enums for dice return code

---
+++ src/dnd_helpers/types/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class DiceReturnCode(Enum):
+    """ DiceReturnCode Enum Class
+
+    Args:
+        Enum (_type_): Inherits from Enum class
+    """
+
+    DICE_RETURN_CODE_SUCCESS = 0,
+    DICE_RETURN_CODE_FAILURE = 1,
