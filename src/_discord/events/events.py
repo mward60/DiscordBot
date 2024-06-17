3f0160bfac52e3ed17d3d0b4ca1c66314b69415e
cbwolfe94 <cbwolfe94@hotmail.com>
2024-06-17 11:22:04 -0500

    Move events and commands to separate py files to better organize code

---
+++ src/_discord/events/events.py
@@ -0,0 +1,22 @@
+
+from ..defs.defs import *
+from ..guild.guild import *
+
+@discord_client.event
+async def on_ready() -> None:
+    """ Sets the bot to "Playing Dungeons and Dragons"
+    """
+
+    print("Setting bot activity...")
+
+    dnd_discord_game = Game(name = "Dungeons and Dragons", type = 3)
+
+    await discord_client.change_presence(activity = dnd_discord_game, status = Status.idle)
+    print(f"Bot activity set to {dnd_discord_game}.")
+    
+    # 
+    for guild in discord_client.guilds:
+        command_tree.copy_global_to(guild = guild)
+        await command_tree.sync(guild = guild)
+    
+    log_bot_guild_presence(discord_client)
